be61d0daf3a48db02a8badd2537eabf42c71b0ef
Jules Amonith <23395221+examosa@users.noreply.github.com>
2026-06-01 16:37:04 -0400

    Refactor lctl.fish completions for clarity and efficiency

--- completions/lctl.fish
+++ completions/lctl.fish
@@ -3,34 +3,40 @@
 function __lctl_agents
     for file in ~/Library/LaunchAgents/*.plist
         set --local label (path basename --no-extension -- $file)
-        path extension -- $label
+        string match --regex '[^.]+$' $label
         echo $label
     end
 end
 
-# subcommands and descriptions
-set --local cmds \
-"cat:Print plist file contents" \
-"edit:Edit plist file in $EDITOR" \
-"file:Show file" \
-"listdisabled:List disabled agents" \
-"log:Show log" \
-"logfiles:Show log files" \
-"reload:Reload agent" \
-"tail:Tail log" \
-"bootout:Boot out agent" \
-"bootstrap:Bootstrap agent" \
-"disable:Disable agent" \
-"enable:Enable agent" \
-"kickstart:Kickstart agent" \
-"kill:Kill agent" \
-"list:List agents" \
-"print:Print information"
 
-for entry in $cmds
-    set --local parts (string split ':' $entry)
-    complete --command lctl --no-files --arguments $parts[1] --description $parts[2]
+# disable files by default
+complete --command lctl --no-files
+
+# -h|--help
+complete --command lctl --condition __fish_use_subcommand --short-option h --long-option help --description 'Show usage'
+
+set --local cmds \
+    "cat:Print plist file contents" \
+    "edit:Edit plist file in $EDITOR" \
+    "file:Show file" \
+    "listdisabled:List disabled agents" \
+    "log:Show log" \
+    "logfiles:Show log files" \
+    "reload:Reload agent" \
+    "tail:Tail log" \
+    "bootout:Boot out agent" \
+    "bootstrap:Bootstrap agent" \
+    "disable:Disable agent" \
+    "enable:Enable agent" \
+    "kickstart:Kickstart agent" \
+    "kill:Kill agent" \
+    "list:List agents" \
+    "print:Print information"
+
+printf '%s\n' $cmds | while read --delimiter : --local cmd desc
+    complete --command lctl --condition __fish_use_subcommand --arguments $cmd --description $desc
 end
 
-# agent completions for second argument (skip for listdisabled)
-complete --command lctl --condition 'not __fish_seen_subcommand_from listdisabled' --arguments '(__lctl_agents)' --description 'agent'
+# agent completions for second argument (skip for commands that don't take one)
+set --local agent_cmds (string match --groups-only --regex '^(?!listdisabled)([^:]+)' $cmds)
+complete --command lctl --condition "__fish_seen_subcommand_from $agent_cmds" --arguments '(__lctl_agents)'
